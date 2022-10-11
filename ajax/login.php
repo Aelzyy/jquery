77761bacc1af1a693b057d4a8596035e5519ec15
Aelzyy <rizkiyono10@gmail.com>
2022-10-11 15:57:58 +0700

    first commit

---
+++ ajax/login.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Login page</title>
+</head>
+<body>
+    <form action="login" action="login.php">
+    <input type="text" id="email" placeholder="Email..."><br>
+    <input type="password" id="password" placeholder="Email..."><br>
+    <input type="submit" value="login" id="login">
+    </form>
+</body>
+</html>
